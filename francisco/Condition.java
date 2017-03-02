42b46c5b79e86e3589399862d2cd5b4b34644485
FranciscoMeloJr <francisco_melo_jr@hotmail.com>
2017-03-02 11:54:23 -0500

    ExePath calling condition functions now

--- francisco/Condition.java
+++ francisco/Condition.java
@@ -44,73 +44,6 @@ public class Condition {
 				arrayRes[array[3]]);
 	}
 
-	// All the conditions are described below -although not enhanced, this is
-	// much easy to understand:
-
-	static int cond1[] = new int[] { 0, 0, 3, 0 };
-	static int cond2[] = new int[] { 0, 0, 3, 1 };
-	static int cond3[] = new int[] { 1, 0, 3, 0 };
-	static int cond4[] = new int[] { 1, 0, 3, 1 };
-	static int cond5[] = new int[] { 2, 0, 3, 1 };
-	static int cond6[] = new int[] { 0, 2, 1, 1 };
-	static int cond7[] = new int[] { 1, 2, 2, 1 };
-	static int cond8[] = new int[] { 0, 2, 2, 0 };
-	static int cond9[] = new int[] { 7, 2, 3, 1 };
-	static int cond10[] = new int[] { 7, 1, 6, 1 };
-	static int cond11[] = new int[] { 7, 2, 4, 1 };
-	static int cond12[] = new int[] { 7, 2, 5, 1 };
-	static int cond13[] = new int[] { 7, 2, 6, 0 };
-	static int cond14[] = new int[] { 10, 1, 1, 0 };
-	static int cond15[] = new int[] { 10, 1, 1, 1 };
-	static int cond16[] = new int[] { 2, 0, 3, 0 };
-	static int cond17[] = new int[] { 7, 2, 5, 0 };
-	static int cond18[] = new int[] { 9, 1, 0, 1 };
-	static int cond19[] = new int[] { 0, 2, 2, 1 };
-	static int cond20[] = new int[] { 8, 0, 2, 0 };
-	static int cond21[] = new int[] { 7, 2, 5, 0 };
-	static int cond22[] = new int[] { 9, 1, 0, 0 };
-	static int cond23[] = new int[] { 7, 2, 6, 1 };
-	static int cond24[] = new int[] { 7, 2, 4, 0 };
-	static int cond25[] = new int[] { 8, 1, 2, 1 };
-	static int cond26[] = new int[] { 1, 0, 2, 1 };
-	static int cond27[] = new int[] { 2, 0, 2, 1 };
-	static int cond28[] = new int[] { 9, 0, 0, 1 };
-	static int cond30[] = new int[] { 8, 1, 2, 0 };
-	static int cond31[] = new int[] { 1, 2, 2, 0 };
-	static int cond32[] = new int[] { 0, 2, 1, 0 };
-	static int cond33[] = new int[] { 10, 0, 1, 1 };
-	static int cond34[] = new int[] { 7, 1, 3, 1 };
-	static int cond35[] = new int[] { 7, 1, 6, 0 };
-	static int cond36[] = new int[] { 7, 2, 2, 0 };
-	static int cond37[] = new int[] { 7, 2, 3, 0 };
-	static int cond38[] = new int[] { 8, 0, 2, 1 };
-	static int cond39[] = new int[] { 10, 0, 1, 0 };
-	static int cond40[] = new int[] { 8, 0, 0, 0 };
-	static int cond41[] = new int[] { 0, 0, 2, 1 };
-
-	// the arrays below describe each one of the paths:
-	static int array14[][] = new int[][] { cond1, cond4, cond5, cond7, cond8, cond9, cond11, cond12, cond13, cond15 };
-
-	static int array13[][] = new int[][] { cond2, cond4, cond5, cond6, cond7, cond8, cond9, cond10, cond11, cond12,
-			cond13, cond14 };
-
-	static int array12[][] = new int[][] { cond2, cond4, cond5, cond6, cond19, cond31, cond9, cond10, cond11, cond21,
-			cond18, cond23 };
-
-	static int array11[][] = new int[][] { cond2, cond4, cond5, cond6, cond19, cond31, cond9, cond10, cond11, cond21,
-			cond22 };
-
-	static int array10[][] = new int[][] { cond2, cond4, cond5, cond7, cond19, cond32, cond9, cond10, cond24, cond25,
-			cond12, cond23 };
-	static int array9[][] = new int[][] { cond2, cond4, cond5, cond7, cond19, cond32, cond23, cond10, cond24, cond30 };
-	static int array8[][] = new int[][] { cond2, cond4, cond5, cond32, cond31, cond8, cond34, cond35 };
-	static int array7[][] = new int[][] { cond41, cond26, cond27, cond6, cond7, cond19, cond36, cond8, cond28, cond33 };
-	static int array6[][] = new int[][] { cond2, cond4, cond5, cond6, cond7, cond19, cond37, cond8, cond28, cond39 };
-	static int array5[][] = new int[][] { cond2, cond4, cond5, cond6, cond7, cond19, cond37, cond8, cond40 };
-	static int array4[][] = new int[][] { cond2, cond4, cond5, cond6, cond7, cond19, cond37, cond20 };
-	static int array3[][] = new int[][] { cond2, cond4, cond16 };
-	static int array2[][] = new int[][] { cond2, cond3 };
-	static int array1[][] = new int[][] { cond1 };
 
 	/**
 	 * This function creates all the paths:
@@ -149,4 +82,168 @@ public class Condition {
 		return branches;
 	}
 
+	float[] calculateTestDataByCondition( float[] testData, int triang, FitnessFunction fit) {
+		int Total = 0;
+		//Condition condition = currentPath.get(i);
+		int a = 0;
+		int b = 0;
+		a = calculateA(testData);
+		b = calculateB(testData);
+
+		if (fit.evaluateCondition(this.op, this.result, a, b) != 0) {
+			Total = fit.evaluateCondition(this.op, this.result, a, b);
+			testData = updateTestData(testData, Total);
+			testData = updateTestData2(testData, Total);
+		}
+		triang = triangFunction(triang);
+		
+		return testData;
+		
+	}
+	/**
+	 * Returns triangFunction
+	 * 
+	 * @param condition
+	 * 
+	 * @param triang
+	 * 
+	 * @return int triang
+	 */
+	private int triangFunction(int triang) {
+
+		int x = 0;
+		for (int i = 1; i < 4; i++) {
+			if (i == 1)
+				x = 1;
+			if (i == 2)
+				x = 2;
+			if (i == 3)
+				x = 2;
+			if (this.op == arrayOpt[2] && this.left == arrayVariables[0]
+					&& this.right == arrayVariables[x] && this.result == arrayRes[0]) {
+				triang = triang + i;
+			}
+		}
+		return triang;
+	}
+	/**
+	 * updateTestData2
+	 * 
+	 * @param condition
+	 * 
+	 * @param testData
+	 * 
+	 * @param Total
+	 * 
+	 * @return testData
+	 */
+	private float[] updateTestData2(float[] testData, int Total) {
+
+		int j = 0;
+		for (int i = 7; i < 10; i++) {
+			if (i == 7)
+				j = 2;
+			if (i == 8)
+				j = 0;
+			if (i == 9)
+				j = 1;
+
+			if (this.left == arrayVariables[i]) {
+				if ((this.op == arrayOpt[1] && this.result == arrayRes[0])
+						|| (this.op == arrayOpt[1] && this.result == arrayRes[1])) {
+					testData[j] += Total;
+				}
+				if ((this.op == arrayOpt[1] && this.result == arrayRes[0])
+						|| (this.op == arrayOpt[1] && this.result == arrayRes[1])) {
+					testData[j] -= Total;
+				}
+			}
+		}
+		return testData;
+	}
+	/**
+	 * updateTestData
+	 * 
+	 * @param testData
+	 * 
+	 * @param condition
+	 * 
+	 * @param Total
+	 * 
+	 * @return new float testData
+	 */
+	private float[] updateTestData(float[] testData, int Total) {
+
+		int max = 4;
+		for (int i = 0; i < max; i++) {
+			if (this.left == arrayVariables[i] && this.right == arrayVariables[3]) {
+				testData[i] -= Total;
+			}
+		}
+
+		for (int i = 0; i < 3; i++) {
+			if (this.left == arrayVariables[0] && this.right == arrayVariables[i]) {
+				if (testData[0] < testData[i]) {
+					testData[0] += Total;
+				} else {
+					testData[i] += Total;
+				}
+			}
+		}
+
+		int x = 1;
+		int y = 1;
+
+		if (this.left == arrayVariables[x] && this.right == arrayVariables[y]) {
+			if (testData[x] < testData[y]) {
+				testData[x] += Total;
+			} else {
+				testData[y] += Total;
+			}
+		}
+		return testData;
+	}	/**
+	 * calculateB
+	 * 
+	 * @param testData
+	 * 
+	 * @param condition
+	 * 
+	 * @return returns b
+	 */
+	private int calculateB(float[] testData) {
+		int b = 0;
+		int[] arrayResult = new int[] { (int) testData[0], (int) testData[1], (int) testData[2], 0, 1, 2, 3 };
+		int max = 7;
+		for (int i = 0; i < max; i++) {
+			b = arrayResult[i];
+		}
+
+		return b;
+	}
+	/**
+	 * calculateB
+	 * 
+	 * @param testData
+	 * 
+	 * @param condition
+	 * 
+	 * @return returns a
+	 */
+	private int calculateA( float[] testData) {
+		int triang = 0;
+		int a = 0;
+		int max = 7;
+		int[] arrayResult = new int[] { (int) testData[0], (int) testData[1], (int) testData[2], (int) triang,
+				(int) (testData[0] + testData[1]), (int) (testData[1] + testData[2]),
+				(int) (testData[0] + testData[2]) };
+		Variables[] array = new Variables[] { Variables.Vars1, Variables.Vars2, Variables.Vars3, Variables.third,
+				Variables.Vartrian, Variables.Vars1s2, Variables.Vars2s3, Variables.Vars1s3 };
+		for (int i = 0; i < max; i++) {
+			if (this.left == array[i]) {
+				a = arrayResult[i];
+			}
+		}
+		return a;
+	}
 }
